d11abb7330e0fbbb9991dd9678491a5939b9a27d
mati <jmsalinas@uc.cl>
2018-11-15 19:35:26 -0300

    update js

--- _assets/js/script5.js
+++ _assets/js/script5.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	$(".social__link").on("click", function(e){
 		e.preventDefault();
-		var aparent = $(this).parents("a");
-		var img = $(aparent).children("img");
-		var alertaalt = $(img).attr("alt");
-		alert(alertaalt);
 		// e.stopPropagation();
+		var aparent = $(this).parents("a").children("img").attr("alt");
+		alert(aparent);
 	})
 	$(".social__element").on("click", ".social__like", function(e){
 		e.preventDefault();
-		$(this).toggleClass("social__like--clicked");
 		// e.stopPropagation();
+		$(this).toggleClass("social__like--clicked");
 	})
 })
